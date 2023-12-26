d1dfca7436d05aca30af858f3f5d30a04551845c
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-12-26 20:49:28 -0300

    feat: add handleClick function

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,10 @@
-function Card({name, url}) {
+function Card({name, url, id, receiveId}) {
+  const handleClick = (e) => {
+    receiveId(e.currentTarget.id) 
+  }  
+  
   return (
-    <article className="container__card">
+    <article className="container__card" id={id} onClick={handleClick} >
       <figure className="card__figure">
         <img src={url} alt={name}></img>
         <figcaption>{name}</figcaption>
