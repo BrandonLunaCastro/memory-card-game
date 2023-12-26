41b221bc32391587b81f2423c34e0745bf01e432
Brandon luna <blunacastro2003@gmail.com>
2023-12-26 12:39:30 -0300

    feat: create component counter

---
+++ src/components/Counter.jsx
@@ -0,0 +1,10 @@
+function Counter({current, best}) {
+  return (
+    <section>
+      <p>Current Score: {current}</p>
+      <p>Best score: {best}</p>
+    </section>
+  )
+}
+
+export default Counter
