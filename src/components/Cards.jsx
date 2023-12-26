a518f0384d0936695fda8ce6e50ee1f53a0d605e
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-12-26 20:49:50 -0300

    feat: add function receiveId with prop

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,11 @@
-
 import Card from "./Card";
 
-function Cards({data}) {
+function Cards({data, receiveId}) {
   return (
     <section className="container">
       {data.length !== 0 &&
-        data.slice(0, 12).map((obj) => {
-          return <Card name={obj.name} url={obj.image} key={obj.id} />;
+        data.map((obj) => {
+          return <Card name={obj.name} url={obj.image} key={obj.id} id={obj.id} receiveId={receiveId}/>;
         })}
     </section>
   );
