d85265d16622e7fa9870d982f331edbdb3d22885
Brandon luna <blunacastro2003@gmail.com>
2023-12-26 12:38:20 -0300

    feat: create state for score and fetch data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,25 @@
-// import { useState } from 'react'
+import { useState, useEffect } from "react";
 import Cards from "./Cards";
 import Header from "./Header";
+import { dataFetch } from "../services/dataFetch";
+import Counter from "./Counter";
 
 function App() {
+  const [currentScore, setScore] = useState(0)
+  const [bestScore, setBest] = useState(0)
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    dataFetch().then((response) => {
+      setData(response.results);
+    });
+  }, []);
+
   return (
     <>
       <Header text={"The Boys memory game"}/>
-      <Cards />
+      <Counter current={currentScore} best={bestScore}/>
+      <Cards data={data}/>
     </>
   ); 
 }
