1798f4bd236c317480530461da7269e3fa4cedef
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-12-26 20:49:08 -0300

    feat: add useEffect to random sort of fetch data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,25 +3,49 @@ import Cards from "./Cards";
 import Header from "./Header";
 import { dataFetch } from "../services/dataFetch";
 import Counter from "./Counter";
+import { verifyArray } from "../services/verifyArray";
+import Wins from "./Wins";
 
 function App() {
-  const [currentScore, setScore] = useState(0)
-  const [bestScore, setBest] = useState(0)
+  const [currentScore, setScore] = useState(0);
+  const [bestScore, setBest] = useState(0);
   const [data, setData] = useState([]);
+  const [arrId, setId] = useState([]);
 
   useEffect(() => {
     dataFetch().then((response) => {
-      setData(response.results);
+      const random = response.results
+        .slice(0, 12);
+      setData(random);
     });
   }, []);
 
+  useEffect(() => {
+    setData(() => data.sort((a, b) => Math.random() - 0.5))
+  }, [arrId, data])
+
+  const receiveId = (value) => {
+    if ( !verifyArray(arrId,value) ) {
+      setId(prevState => [...prevState,value]);
+      setScore(prevScore => prevScore + 1);
+      setBest(prevBest => prevBest > currentScore 
+          ?  prevBest
+          : prevBest + 1
+      )
+    } else {
+      setScore(0);
+      setId([]);
+    }
+  }
+
   return (
     <>
-      <Header text={"The Boys memory game"}/>
-      <Counter current={currentScore} best={bestScore}/>
-      <Cards data={data}/>
+      <Wins />
+      <Header text={"The Boys memory game"} />
+      <Counter current={currentScore} best={bestScore} />
+      <Cards data={data} receiveId={receiveId}/>
     </>
-  ); 
+  );
 }
 
 export default App;
