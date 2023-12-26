a4a621f398a84c5c5bf51b74eb986b9e995bd483
Brandon luna <blunacastro2003@gmail.com>
2023-12-26 12:39:19 -0300

    feat: create component and receive data for the API

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,9 +1,13 @@
-import { dataFetch } from "../services/dataFetch";
-function Cards() {
-  dataFetch().then(res => console.log(res));
+
+import Card from "./Card";
+
+function Cards({data}) {
   return (
-    <section className="container">                           
-    
+    <section className="container">
+      {data.length !== 0 &&
+        data.slice(0, 12).map((obj) => {
+          return <Card name={obj.name} url={obj.image} key={obj.id} />;
+        })}
     </section>
   );
 }
