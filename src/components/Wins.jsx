8a9659d89f6efb63ac33502093b05b2b32c8570c
Brandon luna <blunacastro2003@gmail.com>
2023-12-27 12:36:45 -0300

    feat: add function handleClick

--- src/components/Wins.jsx
+++ src/components/Wins.jsx
@@ -1,11 +1,16 @@
 import gift from "../assets/homelander.gif";
-function Wins() {
+function Wins({restartGame}) {
+
+  const handleClick = () => {
+    restartGame(true)
+  }
+
   return (
     <div className="modal"> 
       <article className="modal__content">
         <p> Congratulations you win!! </p>
         <img src={gift} alt="homelander gift"></img>
-        <button>Restart Game</button>
+        <button onClick={handleClick}>Restart Game</button>
       </article>
     </div>
   );
