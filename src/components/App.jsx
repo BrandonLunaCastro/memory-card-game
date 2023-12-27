26f8e1e5666ba94aedea090bedc4f9993129e026
Brandon luna <blunacastro2003@gmail.com>
2023-12-27 12:35:54 -0300

    feat: add initial states
    BREAKING CHANGE: add restartGame function to show when players wins

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,44 +6,57 @@ import Counter from "./Counter";
 import { verifyArray } from "../services/verifyArray";
 import Wins from "./Wins";
 
+const initialScore = 0;
+const initialBestScore = 0;
+const initialData = [];
+const initialArrId = [];
+
 function App() {
-  const [currentScore, setScore] = useState(0);
-  const [bestScore, setBest] = useState(0);
-  const [data, setData] = useState([]);
-  const [arrId, setId] = useState([]);
+  const [currentScore, setScore] = useState(initialScore);
+  const [bestScore, setBest] = useState(initialBestScore);
+  const [data, setData] = useState(initialData);
+  const [arrId, setId] = useState(initialArrId);
 
   useEffect(() => {
     dataFetch().then((response) => {
-      const random = response.results
-        .slice(0, 12);
+      const random = response.results.slice(0, 12);
       setData(random);
     });
   }, []);
 
   useEffect(() => {
-    setData(() => data.sort((a, b) => Math.random() - 0.5))
-  }, [arrId, data])
+    setData(() => data.sort((a, b) => Math.random() - 0.5));
+  }, [arrId, data]);
 
   const receiveId = (value) => {
-    if ( !verifyArray(arrId,value) ) {
-      setId(prevState => [...prevState,value]);
-      setScore(prevScore => prevScore + 1);
-      setBest(prevBest => prevBest > currentScore 
-          ?  prevBest
-          : prevBest + 1
-      )
+    if (!verifyArray(arrId, value)) {
+      setId((prevState) => [...prevState, value]);
+      setScore((prevScore) => prevScore + 1);
+      setBest((prevBest) =>
+        prevBest > currentScore ? prevBest : prevBest + 1
+      );
     } else {
       setScore(0);
       setId([]);
     }
-  }
+  };
+
+  const restartGame = (value) => {
+    if (value) {
+      setScore(initialScore);
+      setBest(initialBestScore);
+      setId(initialArrId);
+    }
+  };
 
   return (
     <>
-      <Wins />
+      {currentScore === 12 && bestScore === 12 && (
+        <Wins restartGame={restartGame} />
+      )}
       <Header text={"The Boys memory game"} />
       <Counter current={currentScore} best={bestScore} />
-      <Cards data={data} receiveId={receiveId}/>
+      <Cards data={data} receiveId={receiveId} />
     </>
   );
 }
