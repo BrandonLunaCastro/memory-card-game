a68cb8b05cc7be585f2534e506135d119751df78
Brandon luna <blunacastro2003@gmail.com>
2023-12-27 12:36:31 -0300

    feat: add article to show back for the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,24 @@
-function Card({name, url, id, receiveId}) {
+import logo from "../assets/the-boys-pngthe-boys-logo-png.png";
+function Card({ name, url, id, receiveId }) {
   const handleClick = (e) => {
-    receiveId(e.currentTarget.id) 
-  }  
-  
+    receiveId(e.currentTarget.id);
+  };
+
   return (
-    <article className="container__card" id={id} onClick={handleClick} >
-      <figure className="card__figure">
-        <img src={url} alt={name}></img>
-        <figcaption>{name}</figcaption>
-      </figure>
-    </article>
-  )
+    <>
+      <article className="container__card" id={id} onClick={handleClick}>
+        <figure className="flip-card-front">
+          <img src={url} alt={name}></img>
+          <figcaption>{name}</figcaption>
+        </figure>
+      </article>
+      <article>
+        <figure className="flip-card-back">
+          <img src={logo}></img>
+        </figure>
+      </article>
+    </>
+  );
 }
 
 export default Card;
