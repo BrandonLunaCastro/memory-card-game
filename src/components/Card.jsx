d40136db8325132d75e9074e6d45566259aa3f4c
Brandon luna <blunacastro2003@gmail.com>
2023-12-28 12:08:08 -0300

    feat: import react tilt

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,24 @@
 import logo from "../assets/the-boys-pngthe-boys-logo-png.png";
-function Card({ name, url, id, receiveId}) {
+import { Tilt } from "react-tilt";
+
+function Card({ name, url, id, receiveId }) {
   const handleClick = (e) => {
     receiveId(e.currentTarget.id);
   };
 
+  const defaultOptions = {
+    reverse: false,
+    max: 35,
+    perspective: 1000,
+    scale: 1.1,
+    transition: true,
+    axis: null,
+    reset: true,
+    easing: "cubic-bezier(.03,.98,.52,.99)",
+  };
+
   return (
-    <>
+    <Tilt options={defaultOptions}>
       <article className="container__card" id={id} onClick={handleClick}>
         <article className="flip-card-front">
           <img src={url} alt={name}></img>
@@ -15,7 +28,7 @@ function Card({ name, url, id, receiveId}) {
           <img src={logo}></img>
         </div>
       </article>
-    </>
+    </Tilt>
   );
 }
 
