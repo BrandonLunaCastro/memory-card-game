4fda7def16da610787b4679f8712d19ac70bf7ee
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-12-27 21:51:29 -0300

    fixe: refactor component card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import logo from "../assets/the-boys-pngthe-boys-logo-png.png";
-function Card({ name, url, id, receiveId }) {
+function Card({ name, url, id, receiveId}) {
   const handleClick = (e) => {
     receiveId(e.currentTarget.id);
   };
@@ -7,15 +7,13 @@ function Card({ name, url, id, receiveId }) {
   return (
     <>
       <article className="container__card" id={id} onClick={handleClick}>
-        <figure className="flip-card-front">
+        <article className="flip-card-front">
           <img src={url} alt={name}></img>
           <figcaption>{name}</figcaption>
-        </figure>
-      </article>
-      <article>
-        <figure className="flip-card-back">
+        </article>
+        <div className="flip-card-back">
           <img src={logo}></img>
-        </figure>
+        </div>
       </article>
     </>
   );
