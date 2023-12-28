70ecbb09b5a4373dddd945abe2fe5d427d939af8
Brandon luna <blunacastro2003@gmail.com>
2023-12-28 12:08:29 -0300

    fix: format document

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,15 +1,20 @@
 import Card from "./Card";
 
-function Cards({data, receiveId}) {
+function Cards({ data, receiveId }) {
   return (
     <section className="container">
       {data.length !== 0 &&
         data.map((obj) => {
           return (
-            <div key={obj.id}>
-              <Card name={obj.name} url={obj.image} id={obj.id} className={"flip"} receiveId={receiveId}/>
-            </div>
-          ) 
+            <Card
+              key={obj.id}
+              name={obj.name}
+              url={obj.image}
+              id={obj.id}
+              className={"flip"}
+              receiveId={receiveId}
+            />
+          );
         })}
     </section>
   );
